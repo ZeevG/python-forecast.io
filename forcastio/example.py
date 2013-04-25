761a83f8b73a01a98211c070ac8a458a70b1f93d
ZeevG <zeev.gil@gmail.com>
2013-04-25 20:01:17 +0800

    Made example a bit neater

--- forcastio/example.py
+++ forcastio/example.py
@@ -1,46 +1,24 @@
 from forcastio import Forcastio
 
 def main():
-    forcast = Forcastio("ad817e0a2cb6187bca0ab7e3f648428c")
-    print forcast.loadForcast(-32.008,115.806, lazy=False)
+    forcast = Forcastio("YOUR API KEY")
+    print forcast.loadForcast(-32.008,115.806)
 
     print "===========hour========="
     byHour = forcast.getHourly()
     print byHour.summary
     
-    for hourlyData in byHour.data:
-        print hourlyData
-
-    print "===========hour========="
-    byHour = forcast.getHourly()
-    print byHour.summary
-    
-    for hourlyData in byHour.data:
-        print hourlyData
+    for hourlyDataPoint in byHour.data:
+        print hourlyDataPoint
 
     print "===========daily========="
-    byHour = forcast.getDaily()
-    print byHour.summary
+    byDay = forcast.getDaily()
+    print byDay.summary
     
-    for hourlyData in byHour.data:
-        print hourlyData
-
-    print "===========daily========="
-    byHour = forcast.getDaily()
-    print byHour.summary
-    
-    for hourlyData in byHour.data:
-        print hourlyData
+    for dailyDataPoint in byDay.data:
+        print dailyDataPoint
 
 
-    '''temp = 0
-    i = 0
-    for hour in forcast.getHourly().data:
-        print hour.temperature
-        i = i+1
-        temp = temp + hour.temperature
-
-    print temp/i'''
     
 if __name__ == "__main__":
     main()
