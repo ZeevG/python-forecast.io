93542598ab4f760dd28da89aa97826332cb363fc
Tobias Sauerwein <cgtobi@gmail.com>
2018-03-23 19:17:51 +0000

    Update doc string.

--- forecastio/api.py
+++ forecastio/api.py
@@ -12,18 +12,18 @@ def load_forecast(key, lat, lng, time=None, units="auto", lang="en", lazy=False,
         This function builds the request url and loads some or all of the
         needed json depending on lazy is True
 
-        inLat:  The latitude of the forecast
-        inLong: The longitude of the forecast
-        time:   A datetime.datetime object representing the desired time of
-               the forecast. If no timezone is present, the API assumes local
-               time at the provided latitude and longitude.
-        units:  A string of the preferred units of measurement, "auto" id
-                default. also us,ca,uk,si is available
-        lang:   Return summary properties in the desired language
-        lazy:   Defaults to false.  The function will only request the json
-                data as it is needed. Results in more requests, but
-                probably a faster response time (I haven't checked)
-        timeout:The timeout in seconds. Defaults to 3 seconds.
+        inLat:   The latitude of the forecast
+        inLong:  The longitude of the forecast
+        time:    A datetime.datetime object representing the desired time of
+                 the forecast. If no timezone is present, the API assumes local
+                 time at the provided latitude and longitude.
+        units:   A string of the preferred units of measurement, "auto" id
+                 default. also us,ca,uk,si is available
+        lang:    Return summary properties in the desired language
+        lazy:    Defaults to false.  The function will only request the json
+                 data as it is needed. Results in more requests, but
+                 probably a faster response time (I haven't checked)
+        timeout: The timeout in seconds. Defaults to 3 seconds.
     """
 
     TIMEOUT = timeout
