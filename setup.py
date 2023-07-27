073e359dd8233d21fc20a6cd43e352b552cddfce
krazos <krazos@gmail.com>
2023-07-27 09:11:24 -0400

    Require responses<=0.23.1
    Require responses<=0.23.1 to resolve dependency conflicts (e.g., with Home Assistant integrations) pending upstream resolution
    
    https://github.com/getsentry/responses/issues/657
    https://github.com/home-assistant/core/issues/97248

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setup(
     packages=['forecastio'],
     package_data={'forecastio': ['LICENSE.txt', 'README.rst']},
     long_description=open('README.rst').read(),
-    install_requires=['requests>=1.6', 'responses'],
+    install_requires=['requests>=1.6', 'responses<=0.23.1'],
 )
